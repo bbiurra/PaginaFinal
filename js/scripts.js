3ad355216021acaa46d2200d927ca027e34049a7
zabatsuuy <>
2017-10-13 19:26:40 -0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,136 @@
+var clic1 = document.getElementById("texto1");
+var clic2 = document.getElementById("texto2");
+
+function mostrar(){
+    if(clic1.style.display == "block"){
+        clic1.style.display = "none";
+    } else {
+        clic1.style.display = "block";
+    }
+}
+
+function Mostrar(){
+    if(clic2.style.display == "block"){
+        clic2.style.display = "none";
+    } else {
+        clic2.style.display = "block";
+    }
+}
+
+/*Botón desarrollo web*/
+var desarrollo = document.getElementById("web");
+
+function mostrarweb(){
+    if(desarrollo.style.display == "block"){
+        desarrollo.style.display = "none";
+    } else {
+        desarrollo.style.display = "block";
+    }
+}
+
+/*Botones de menú de Desarrollo Web*/
+var botones = document.getElementById("botones");
+
+function aparece(){
+    if (botones.style.display == "block"){
+        botones.style.display = "none";
+    } else {
+        botones.style.display = "block";
+    }
+}
+
+var botones1 = document.getElementById("botones1");
+
+function aparece1(){
+    if (botones1.style.display == "block"){
+        botones1.style.display = "none";
+    } else {
+        botones1.style.display = "block";
+    }
+}
+
+var botones2 = document.getElementById("botones2");
+
+function aparece2(){
+    if (botones2.style.display == "block"){
+        botones2.style.display = "none";
+    } else {
+        botones2.style.display = "block";
+    }
+}
+var botones3 = document.getElementById("botones3");
+
+function aparece3(){
+    if (botones3.style.display == "block"){
+        botones3.style.display = "none";
+    } else {
+        botones3.style.display = "block";
+    }
+}
+
+var botones4 = document.getElementById("botones4");
+
+function aparece4(){
+    if (botones4.style.display == "block"){
+        botones4.style.display = "none";
+    } else {
+        botones4.style.display = "block";
+    }
+}
+var botones5 = document.getElementById("botones5");
+
+function aparece5(){
+    if (botones5.style.display == "block"){
+        botones5.style.display = "none";
+    } else {
+        botones5.style.display = "block";
+    }
+}
+var botones6 = document.getElementById("botones6");
+
+function aparece6(){
+    if (botones6.style.display == "block"){
+        botones6.style.display = "none";
+    } else {
+        botones6.style.display = "block";
+    }
+}
+var botones7 = document.getElementById("botones7");
+
+function aparece7(){
+    if (botones7.style.display == "block"){
+        botones7.style.display = "none";
+    } else {
+        botones7.style.display = "block";
+    }
+}
+var botones8 = document.getElementById("botones8");
+
+function aparece8(){
+    if (botones8.style.display == "block"){
+        botones8.style.display = "none";
+    } else {
+        botones8.style.display = "block";
+    }
+}
+var botones9 = document.getElementById("botones9");
+
+function aparece9(){
+    if (botones9.style.display == "block"){
+        botones9.style.display = "none";
+    } else {
+        botones9.style.display = "block";
+    }
+}
+
+/*Botón Competencias Transversales*/
+var compet = document.getElementById("competencias");
+
+function mostrarcompetencias(){
+    if(compet.style.display == "block"){
+        compet.style.display = "none";
+    } else {
+        compet.style.display = "block";
+    }
+}
+
